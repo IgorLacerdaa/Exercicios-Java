021e1243b0cebb67b091d02af188af398a2c23dd
IgorLacerdaa <144690368+IgorLacerdaa@users.noreply.github.com>
2025-08-20 22:49:59 -0300

    main.java

--- Atividade1/main.java
+++ Atividade1/main.java
@@ -2,33 +2,21 @@ package teste;
 
 import java.util.Scanner;
 
-public class main {
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Digite a nota 1: ");
-		 double n1 = scanner.nextDouble();
-		
-		System.out.print("Digite a nota 2: ");
-		 double n2 = scanner.nextDouble();
-		 
-		 System.out.print("Digite a nota 3: ");
-		 double n3 = scanner.nextDouble();
-		 
-		 
-		 double media = (n1 * 0.40) + (n2 * 0.40) + (n3 * 0.20);
-		System.out.println("Sua nota 1 foi: " + n1);
-		System.out.println("Sua nota 2 foi: " + n2);
-		System.out.println("Sua nota 3 foi: " + n3);
-		
-		System.out.println("Sua media foi: " + media);
-		if(media >= 6) {
-			System.out.print("Parabens!! Voce foi aprovado");
-		} else {
-			System.out.print("Infelizmente voce nao foi aprovado");
-		}
+        System.out.print("Digite a nota 1: ");
+        double n1 = scanner.nextDouble();
 
-	}
+        System.out.print("Digite a nota 2: ");
+        double n2 = scanner.nextDouble();
 
+        System.out.print("Digite a nota 3: ");
+        double n3 = scanner.nextDouble();
+
+        double media = (n1 + n2 + n3) / 3;
+
+        System.out.println("Sua média foi: " + media);
+    }
 }
